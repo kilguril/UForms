d63c68d56a55ffac4659b9840dba518025ed1c75
martinc <martin@282productions.com>
2014-10-28 07:55:17 +0200

    - Add Rect field to Image control - Created test project for building library

--- Assets/UForms/Editor/Controls/Image.cs
+++ Assets/UForms/Editor/Controls/Image.cs
@@ -10,29 +10,33 @@ namespace UForms.Controls
         public Material     DrawMaterial    { get; set; }
         public bool         DrawTransparent { get; set; }
         public bool         DrawAlpha       { get; set; }
-        public ScaleMode    Scale { get; set; }
+        public ScaleMode    Scale           { get; set; }
+
+        public Rect?        TexCoords       { get; set; }
 
         protected override Vector2 DefaultSize {
             get { return new Vector2( 64.0f, 64.0f ); }
         }
 
-        public Image( Texture tex = null, ScaleMode scaleMode = ScaleMode.ScaleToFit, bool drawAlpha = false, bool drawTransparent = true, Material mat = null ) : base()
+        public Image( Texture tex = null, Rect? texCoords = null, ScaleMode scaleMode = ScaleMode.ScaleToFit, bool drawAlpha = false, bool drawTransparent = true, Material mat = null ) : base()
         {
             DrawTexture     = tex;
             Scale           = scaleMode;
             DrawAlpha       = drawAlpha;
             DrawTransparent = drawTransparent;
             DrawMaterial    = mat;
+            TexCoords       = texCoords;
         }
 
 
-        public Image( Vector2 position, Vector2 size, Texture tex = null, ScaleMode scaleMode = ScaleMode.ScaleToFit, bool drawAlpha = false, bool drawTransparent = true, Material mat = null ) : base( position, size )
+        public Image( Vector2 position, Vector2 size, Texture tex = null, Rect? texCoords = null, ScaleMode scaleMode = ScaleMode.ScaleToFit, bool drawAlpha = false, bool drawTransparent = true, Material mat = null ) : base( position, size )
         {
-            DrawTexture = tex;
-            Scale = scaleMode;
-            DrawAlpha = drawAlpha;
-            DrawTransparent = drawTransparent;
-            DrawMaterial = mat;
+            DrawTexture         = tex;
+            Scale               = scaleMode;
+            DrawAlpha           = drawAlpha;
+            DrawTransparent     = drawTransparent;
+            DrawMaterial        = mat;
+            TexCoords           = texCoords;
         }
 
         protected override void OnDraw()
@@ -48,13 +52,20 @@ namespace UForms.Controls
             }
             else
             {
-                if ( DrawTransparent )
+                if ( TexCoords != null )
                 {
-                    EditorGUI.DrawTextureTransparent( ScreenRect, DrawTexture, Scale );
+                    GUI.DrawTextureWithTexCoords( ScreenRect, DrawTexture, TexCoords ?? default(Rect), DrawTransparent );
                 }
                 else
                 {
-                    EditorGUI.DrawPreviewTexture( ScreenRect, DrawTexture, DrawMaterial, Scale );
+                    if ( DrawTransparent )
+                    {
+                        EditorGUI.DrawTextureTransparent( ScreenRect, DrawTexture, Scale );
+                    }
+                    else
+                    {
+                        EditorGUI.DrawPreviewTexture( ScreenRect, DrawTexture, DrawMaterial, Scale );
+                    }
                 }
             }
         }

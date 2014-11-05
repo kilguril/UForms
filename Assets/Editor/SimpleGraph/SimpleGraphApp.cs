23744b2dde95b3e27ae4009b0b0e83c241abf1cf
martinc <martin@282productions.com>
2014-11-05 14:53:11 +0200

    - Added ContextMenuControl

--- Assets/Editor/SimpleGraph/SimpleGraphApp.cs
+++ Assets/Editor/SimpleGraph/SimpleGraphApp.cs
@@ -14,7 +14,19 @@ public class SimpleGraphApp : UFormsApplication
         EditorWindow.GetWindow< SimpleGraphApp >();
     }
 
-    private SimpleGraph     m_graph;
+
+    enum ContextActionType
+    {
+        CreateRoot,
+        CreatePassthrough,
+        CreateBranch,
+        CreateOutput
+    }
+
+    private SimpleGraph             m_graph;
+    private ContextMenuControl      m_contextMenu;
+
+    private Vector2                 m_lastMousePosition;    // We'll use this to create nodes from context menu
 
     protected override void OnInitialize()
     {
@@ -22,11 +34,60 @@ public class SimpleGraphApp : UFormsApplication
         m_graph.AddDecorator( new Scrollbars( true, true, true ) );
         m_graph.SetSize( 100.0f, 100.0f, Control.MetricsUnits.Percentage, Control.MetricsUnits.Percentage );
 
-        AddChild( m_graph );
+        m_contextMenu = new ContextMenuControl();
+        m_contextMenu.Menu.AddItem( new GUIContent( "Create Root" ), false, CreateNode, ContextActionType.CreateRoot );
+        m_contextMenu.Menu.AddItem( new GUIContent( "Create Passthrough" ), false, CreateNode, ContextActionType.CreatePassthrough );
+        m_contextMenu.Menu.AddItem( new GUIContent( "Create Branch" ), false, CreateNode, ContextActionType.CreateBranch );
+        m_contextMenu.Menu.AddItem( new GUIContent( "Create Output" ), false, CreateNode, ContextActionType.CreateOutput );
 
-        m_graph.AddNode( new SimpleGraphNode( 0, 1, "Root Node" ) );
-        m_graph.AddNode( new SimpleGraphNode( 1, 1, "Passthrough Node" ) );
-        m_graph.AddNode( new SimpleGraphNode( 1, 4, "Branch Node" ) );
-        m_graph.AddNode( new SimpleGraphNode( 1, 0, "Output Node" ) );
+        AddChild( m_graph );
+        AddChild( m_contextMenu );
+    }
+
+    private void CreateNode( object arg )
+    {        
+        ContextActionType t = ( ContextActionType )arg;
+
+        int     inputs = 0;
+        int     outputs = 0;
+        string  text = "";
+
+        switch( t )
+        {
+            case ContextActionType.CreateRoot:            
+                inputs = 0;
+                outputs = 1;
+                text = "Root";
+            break;
+
+            case ContextActionType.CreatePassthrough:
+                inputs = 1;
+                outputs = 1;
+                text = "Passthrough";
+            break;
+
+            case ContextActionType.CreateBranch:
+                inputs = 1;
+                outputs = 3;
+                text = "Branch";
+            break;
+
+            case ContextActionType.CreateOutput:
+                inputs = 1;
+                outputs = 0;
+                text = "Output";
+            break;
+        }
+
+        SimpleGraphNode node = new SimpleGraphNode( inputs, outputs, text );
+        m_graph.AddNode( node );
+
+        node.SetPosition( m_lastMousePosition );
+    }
+
+    protected override void OnGUI()
+    {
+        m_lastMousePosition = Event.current.mousePosition;
+        base.OnGUI();
     }
 }
---
+++ Assets/UForms/Editor/Controls/ContextMenuControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using UForms.Events;
+
+namespace UForms.Controls
+{
+    public class ContextMenuControl : Control
+    {
+        public GenericMenu Menu { get; private set; }
+
+        public ContextMenuControl(  ) : base()
+        {
+            Menu = new GenericMenu();
+            SetSize( 100.0f, 100.0f, MetricsUnits.Percentage, MetricsUnits.Percentage );
+        }
+
+
+        protected override void OnContextClick( Event e )
+        {
+            if ( ScreenRect.Contains( e.mousePosition ) )
+            {
+                Menu.ShowAsContext();
+                e.Use();
+            }
+        }
+
+    }
+}
